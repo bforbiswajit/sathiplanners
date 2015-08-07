64ec0f3d5c66b0063a37e211ef30ed334121b111
Biswjit Bardhan <sunshine.cst.07@gmail.com>
2015-08-07 19:13:52 +0530

    public folder included

---
+++ public/js/formSubmission.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $("form").submit(function(event){	//GENERIC form submit function
+	event.preventDefault();
+        var form_id = "#" + $(this).closest("form").attr("id");
+	var form_data = new FormData(this);
+	//var form_data = $(this).serialize();
+        var url = $(form_id).attr("action");
+        //console.log("form data - ", form_data);
+        $.ajax({
+            url : url,
+            type : "POST",
+            mimeType: "multipart/form-data",
+            contentType: false,
+            cache: false,
+            processData: false,
+            //headers : {"Api-Key": "1234"},
+            data : form_data,
+            success : function(data){
+                document.write(data);
+                console.log(data);
+                response = JSON.parse(data);
+                /*if(response.status == "success"){
+                    //console.log(response.data[0]);
+                    $(form_id + "_success").text(response.data[0]).fadeIn(2000).fadeOut(2000);
+                }
+                else{
+                    $(form_id + "_danger").text("Error Code #" + response.message.Code + ", " + response.message.Title).fadeIn(2000);
+                }
+                $(form_id + "input[type='reset']").trigger("click");*/
+            },
+            error : function(XMLHttpRequest, textStatus, errorThrown){ 
+                console.log("Status: " + textStatus + ", Error: " + errorThrown); 
+            }
+        });
+    });
+});
---
+++ public/js/plan.js
@@ -0,0 +1,8 @@
+/* 
+ * FirstMe Server API
+ * Author : Biswajit Bardhan  * 
+ */
+$(document).ready(function(){
+    var BASE_URL = "../index.php";
+    
+});
